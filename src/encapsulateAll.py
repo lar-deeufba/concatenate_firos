3015dfe9aec64ea854071a549b663097acba29e0
Henrique Poleselo <hpoleselo@gmail.com>
2019-12-13 15:04:22 -0300

    renamed structure

--- src/encapsulateAll.py
+++ src/encapsulateAll.py
@@ -1,51 +1,36 @@
 import rospy
-
 from sensor_msgs.msg import JointState
-# Colocar esse script de concatenar tudo aonde?
-# Dentro do package do UR5? acho que sim
-from encapsulateAll import BrokerMsg
+from encapsulateAll import UR5State
 
-rospy.Subscriber("")
-rospy.Subscriber("")
-rospy.Subscriber("")
-rospy.Subscriber("")
-rospy.Subscriber("")
-rospy.Subscriber("")
 
-bmsg = BrokerMsg()
-bmsg.skill = 
-bmsg.joint1 =
-bmsg.
+class UR5StatePublisher(object):
 
-     data["Time"]={"value": float(row[0]),"type": "Number"}
-        data["Skill"]={"value": float(row[1]),"type": "Number"}
-        data["Joint1"]={"value": float(row[2]),"type": "Number"}
-        data["Joint2"]={"value": float(row[3]),"type": "Number"}
-        data["Joint3"]={"value": float(row[4]),"type": "Number"}
-        data["Joint4"]={"value": float(row[5]),"type": "Number"}
-        data["Joint5"]={"value": float(row[6]),"type": "Number"}
-        data["Joint6"]={"value": float(row[7]),"type": "Number"}
-        data["Velocity1"]={"value": float(row[8]),"type": "Number"}
-        data["Velocity2"]={"value": float(row[9]),"type": "Number"}
-        data["Velocity3"]={"value": float(row[10]),"type": "Number"}
-        data["Velocity4"]={"value": float(row[11]),"type": "Number"}
-        data["Velocity5"]={"value": float(row[12]),"type": "Number"}
-        data["Velocity6"]={"value": float(row[13]),"type": "Number"}
-        data["X"]={"value": float(row[14]),"type": "Number"}
-        data["Y"]={"value": float(row[15]),"type": "Number"}
-        data["Z"]={"value": float(row[16]),"type": "Number"}
-        data["ax"]={"value": float(row[17]),"type": "Number"}
-        data["ay"]={"value": float(row[18]),"type": "Number"}
-        data["az"]={"value": float(row[19]),"type": "Number"}
-        data["Tx"]={"value": float(row[20]),"type": "Number"}
-        data["Ty"]={"value": float(row[21]),"type": "Number"}
-        data["Tz"]={"value": float(row[22]),"type": "Number"}
-        data["Cx"]={"value": float(row[23]),"type": "Number"}
-        data["Cy"]={"value": float(row[24]),"type": "Number"}
-        data["Cz"]={"value": float(row[25]),"type": "Number"}
-        data["Cax"]={"value": float(row[26]),"type": "Number"}
-        data["Cay"]={"value": float(row[27]),"type": "Number"}
-        data["Caz"]={"value": float(row[28]),"type": "Number"}
-        data["GripperPosition"]={"value": int(row[29]),"type": "Integer"}
-        data["GripperStatus"]={"value": int(row[30]),"type": "Integer"}
+   def __init__(self):
+      self.rate = rospy.Rate(1)
+      self.iotPublisher = rospy.Publisher("ur5_states", UR5State, queue_size=10)
+      self.main()
+
+   def jointStateCallback(self, msg):
+      joint1 = msg.joint1
+      joint2 = msg.joint2
+      return joint1, joint2
+   
+   def anotherTopicFromDaniel(self, msg):
+      return msg
+
+   def main():
+      rospy.Subscriber("joint_states", PrinterState, self.printerProgressCallback)
+      rospy.Subscriber("printer3d1/printer3d", PrinterState, self.printerProgressCallback)
+      bmsg = BrokerMsg()
+      bmsg.Skill = 
+      bmsg.joint1, bmsg.joint2 = self.jointStateCallback() 
+      self.iotPublisher.publish(bmsg)
+
+      # Rate to publish the messages
+      self.rate.sleep()
+      rospy.spin()
+
+
+
+
 
